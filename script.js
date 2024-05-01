edf6860f4b5ffb4b65b409d5ef36417878ee231a
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-05-01 22:55:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-//your code here
-function allowDrop(ev) {
-  ev.preventDefault();
+$(document).ready(function() {
+  $(".draggable").draggable({
+    revert: true,
+    revertDuration: 0,
+    zIndex: 100,
+    cursor: "move"
+  });
 
-let dragindex = 0;
-let dropindex = 0;
-let clone = "";
-
-const images = document.querySelectorAll(".image");
-
-function drag(e) {
-  e.dataTransfer.setData("text", e.target.id);
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-function allowDrop(e) {
-  e.preventDefault();
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
-function drop(e) {
-  clone = e.target.cloneNode(true);
-  let data = e.dataTransfer.getData("text");
-  let nodelist = document.getElementById("parent").childNodes;
-  console.log(data, e.target.id);
-  for (let i = 1; i <= nodelist.length; i++) 
-  {
-    if (nodelist[i].id == data) 
-	{
-      dragindex = 1;
+  $(".draggable").droppable({
+    drop: function(event, ui) {
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = $(this).attr("id");
+      
+      // Swap background images
+      var draggableImage = $("#" + draggableId + " img").attr("src");
+      var droppableImage = $("#" + droppableId + " img").attr("src");
+      $("#" + draggableId + " img").attr("src", droppableImage);
+      $("#" + droppableId + " img").attr("src", draggableImage);
     }
-  }
-
-  dragdrop(clone);
-
-  document.getElementById("parent").replaceChild(document.getElementById(data), e.target);
-
-  document.getElementById("parent").insertBefore(clone,
-      document.getElementById("parent").childNodes[dragindex]
-    );
-}
-
-const dragdrop = (image) => {
-  image.ondragstart = drag;
-  image.ondragover = allowDrop;
-  image.ondrop = drop;
-};
-
-images.forEach(dragdrop);
+  });
+});
