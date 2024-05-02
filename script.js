61d68a744a0d3c22b56eb4c30921306fbcf82951
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-05-02 08:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
 
   $(".draggable").droppable({
     drop: function(event, ui) {
-      var dragId = ui.draggable.attr("id");
+      var draggableId = ui.draggable.attr("id");
       var droppableId = $(this).attr("id");
       
       // Swap background images
-      var draggableImage = $("#" + dragId + " img").attr("src");
+      var draggableImage = $("#" + draggableIdId + " img").attr("src");
       var droppableImage = $("#" + droppableId + " img").attr("src");
-      $("#" + dragId + " img").attr("src", droppableImage);
+      $("#" + draggableIdId + " img").attr("src", droppableImage);
       $("#" + droppableId + " img").attr("src", draggableImage);
     }
   });
